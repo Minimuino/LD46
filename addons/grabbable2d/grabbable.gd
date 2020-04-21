b4d38f08ea2d21cd7bd765b23eef9c78469da3f6
Carlos Perez Ramil <cperez@trileucosolutions.com>
2020-04-21 20:25:57 +0200

    Add gh-pages branch with HTML5 version of the game

--- addons/grabbable2d/grabbable.gd
+++
@@ -1,9 +0,0 @@
-tool
-extends EditorPlugin
-
-func _enter_tree():
-    # Add the new type with a name, a parent type, a script and an icon
-    add_custom_type("Grabbable2D", "RigidBody2D", preload("grabbable-behaviour.gd"), preload("icon.png"))
-
-func _exit_tree():
-    remove_custom_type("Grabbable2D")
